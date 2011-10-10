0543dcb3652b104a324363ac351744c7cfff0ded
patrickgombert <patrickgombert@gmail.com>
2011-10-10 15:11:12 -0500

    Add Simple Post

--- fixtures/http_browser.rb
+++ fixtures/http_browser.rb
@@ -3,19 +3,27 @@ gem 'httparty'
 require "httparty"
 
 module Fixtures
-  
+
   class HttpBrowser
-    attr_accessor :host, :port, :status
+    attr_accessor :host, :port, :status, :data
     attr_reader :response
-    
-    def get(url)    
+
+    def get(url)
       @response = HTTParty.get("http://#{@host}:#{@port}#{url}")
       @message = response.message
       @status =  response.code
     rescue Errno::ECONNREFUSED => e
       @message = "#{e.message}.  Are you sure your server is running on http://#{@host}:#{@port}?"
     end
-    
+
+    def post(url)
+      @response = HTTParty.post("http://#{@host}:#{@port}#{url}", :body => @data)
+      @message = response.message
+      @status = response.code
+    rescue Errno::ECONNREFUSED => e
+      @message = "#{e.message}.  Are you sure your server is running on http://#{@host}:#{@port}?"
+    end
+
     def ok_response
       return @status == 200
     end
