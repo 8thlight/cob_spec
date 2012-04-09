1860083fe91169141431e3f718577f1083ad1017
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-09 10:30:32 -0500

    Adding ability to run non-java server.

--- fixtures/server.rb
+++ fixtures/server.rb
@@ -4,12 +4,25 @@ module Fixtures
     attr_accessor :path, :port, :directory
     
     def start_server
-      @@pid = IO.popen("/usr/bin/java -jar #{@path} -p #{@port} -d #{@directory}").pid
+      @@pid = IO.popen("#{start_command} -p #{@port} -d #{@directory}").pid
       sleep(1)
     end
     
     def stop_server
       Process.kill("INT", @@pid)
     end
+
+
+    private 
+    
+    def start_command
+      if java? then return "/usr/bin/java -jar #{@path}" end
+      @path
+    end
+
+    def java?
+      @path.end_with? ".jar"
+    end
+
   end
 end
