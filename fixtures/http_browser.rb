f60b0b87a421fccac3c1daada145bb6e8841ba30
Nathan Walker <earthworn@gmail.com>
2013-03-19 10:36:50 -0500

    Merged Angeleah's changes. Probably still need to move the tests to the new suite.

--- fixtures/http_browser.rb
+++ fixtures/http_browser.rb
@@ -27,17 +27,9 @@ class HttpBrowser
     response.code == code.to_i
   end
 
-    def method_not_allowed_response
-       return @status == 405
-    end
-
-    def partial_content_response
-       return @status == 206
-    end
-
-    def body_has_content(content)
-      @data.include? content
-    end
+  def body_has_content(content)
+    @data.include? content
+  end
 
   def body_has_directory_contents(directory)
     entries = Dir.entries(directory)
@@ -50,32 +42,28 @@ class HttpBrowser
     not response.body.match(/href=("|')[^'"]*\/#{path}("|')/).nil?
   end
 
-    def read_file(file)
-       File.open(file, 'rb') { |f| f.read }
-    end
+  def read_file(file)
+    File.open(file, 'rb') { |f| f.read }
+  end
 
-    def body_has_file_contents(file)
-      contents = read_file(file)
-      @data.include? contents
-    end
+  def body_has_file_contents(file)
+    contents = read_file(file)
+    response.body.include? contents
+  end
 
-    def get_with_partial_header(url)
-      @response = HTTParty.get("http://#{@host}:#{@port}#{url}", :headers => {"Range: " => "bytes=0-4"})
-      @message = response.message
-      @status = response.code
-      @data = response.body
-      rescue Errno::ECONNREFUSED => e
-       econnrefused e
-    end
+  def get_with_partial_header(url)
+    @response = HTTParty.get("http://#{@host}:#{@port}#{url}", :headers => {"Range: " => "bytes=0-4"})
+    response_present?
+  end
 
-    def body_has_partial_file_contents(file)
-      contents = read_file(file)
-      @data == contents[0..3]
-    end
+  def body_has_partial_file_contents(file)
+    contents = read_file(file)
+    response.body == contents[0..3]
+  end
 
-    def header_field_value(field)
-      return @response.headers[field]
-    end
+  def header_field_value(field)
+    response.headers[field]
+  end
 
   def last_request_path
     response.request.path.to_s
