2dd89514596265226a4b196334afda4e4e490369
Aaron Lahey <aaron.lahey@gmail.com>
2014-07-11 14:37:57 -0500

    adds fixtures for image echo

--- fixtures/websockets.rb
+++ fixtures/websockets.rb
@@ -2,6 +2,7 @@ require "websocket-eventmachine-client"
 
 class Websockets
   attr_accessor :host, :port, :path
+  attr_reader :echoed_file, :echoed_message, :pong_payload
 
   def ping(payload)
     EM.run do
@@ -35,11 +36,35 @@ class Websockets
     end
   end
 
+  def send_file(file_name)
+    file_contents = File.open(file_name, 'rb') { |f| f.read }
+
+    EM.run do
+      ws = WebSocket::EventMachine::Client.connect(:uri => "ws://#{host}:#{port}#{path}")
+
+      ws.onopen do
+        ws.send(file_contents, :type => :binary)
+      end
+
+      ws.onmessage do |message|
+        @echoed_file = message
+
+        EM.stop
+      end
+    end
+  end
+
   def echoed_message_equals(message)
     @echoed_message == message
   end
 
   def pong_payload_equals(payload)
     @pong_payload == payload
+  end
+
+  def echoed_file_was(file_path)
+    file_contents = File.open(file_path, 'rb') { |f| f.read }
+
+    @echoed_file == file_contents
   end
 end
