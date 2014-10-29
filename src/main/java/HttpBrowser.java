5b1c8d76cdd6a83fad58cd0873e3934c080913b2
Eric Smith <esmith@8thlight.com>
2014-10-29 17:11:22 -0500

    HttpBrowser and Server filled out.
    I get to red on the test. The HttpBrowser is not filled out yet.

--- src/main/java/HttpBrowser.java
+++ src/main/java/HttpBrowser.java
@@ -1,2 +1,34 @@
 public class HttpBrowser {
+  private String host;
+  private String port;
+
+  public void setHost(String host) {
+    this.host = host;
+  }
+
+  public void setPort(String port) {
+    this.port = port;
+  }
+
+  public void get(String url) {
+  }
+
+  public void getWithCredentials(String url) {
+  }
+
+  public void put(String url) {
+  }
+
+  public void head(String url) {
+  }
+
+  public boolean responseCodeEquals(int code) {
+    return false;
+  }
+
+  public boolean bodyHasContent(String content) {
+    return false;
+  }
+
+
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,5 +1,28 @@
 public class Server {
+  private String startCommand;
+  private String directory;
+  private String port;
+  private static int pid;
+  private Process process;
 
   public void setStartCommand(String command) {
+    startCommand = command;
+  }
+
+  public void setDirectory(String directory) {
+    this.directory = directory;
+  }
+
+  public void setPort(String port) {
+    this.port = port;
+  }
+
+  public void startServer() throws Exception {
+    process = Runtime.getRuntime().exec(startCommand + " -p " + port + " -d " + directory);
+    Thread.sleep(2);
+  }
+
+  public void stopServer() throws Exception {
+    process.destroy();
   }
 }
