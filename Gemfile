5028aeca316429ae74cc273065ca6cea244579a4
Eric Smith <esmith@8thlight.com>
2014-11-06 17:15:02 -0600

    Remove ruby

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source "http://rubygems.org"
-
-gem 'httparty', '0.11.0'
-gem 'typhoeus', '0.6.3'
-gem 'nokogiri', '1.5.9'
--- fixtures/http_browser_runner.rb
+++
@@ -1,5 +0,0 @@
-require_relative '../fixtures/http_browser'
-
-http_browser = HttpBrowser.new
-http_browser.host = "localhost"
-http_browser.port = 5000
