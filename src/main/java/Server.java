b97e7cd0605b167a333e5f5c03867e3c61cd3ffa
Eric Smith <esmith@8thlight.com>
2014-11-06 09:42:44 -0600

    This gets the tests green up to the directory listing

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,28 +1,29 @@
 public class Server {
-  private String startCommand;
-  private String directory;
-  private String port;
-  private static int pid;
-  private Process process;
+    private String startCommand;
+    private String directory;
+    private String port;
+    private static int pid;
+    static private Process process;
 
-  public void setStartCommand(String command) {
-    startCommand = command;
-  }
+    public void setStartCommand(String command) {
+        startCommand = command;
+    }
 
-  public void setDirectory(String directory) {
-    this.directory = directory;
-  }
+    public void setDirectory(String directory) {
+        this.directory = directory;
+    }
 
-  public void setPort(String port) {
-    this.port = port;
-  }
+    public void setPort(String port) {
+        this.port = port;
+    }
 
-  public void startServer() throws Exception {
-    process = Runtime.getRuntime().exec(startCommand + " -p " + port + " -d " + directory);
-    Thread.sleep(2);
-  }
+    public void startServer() throws Exception {
+        String command = startCommand + " -p " + port + " -d " + directory;
+        process = Runtime.getRuntime().exec(command);
+        Thread.sleep(2000);
+    }
 
-  public void stopServer() throws Exception {
-    process.destroy();
-  }
+    public void stopServer() throws Exception {
+        process.destroy();
+    }
 }
