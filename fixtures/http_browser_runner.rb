f92bd5336a22ed3e668a3d0ca03767700831c5c8
Uku Taht <uku.taht@gmail.com>
2014-11-11 14:42:26 +0000

    add more comprehensive byte serving specs

--- fixtures/http_browser_runner.rb
+++ fixtures/http_browser_runner.rb
@@ -3,6 +3,3 @@ require_relative '../fixtures/http_browser'
 http_browser = HttpBrowser.new
 http_browser.host = "localhost"
 http_browser.port = 5000
-
-http_browser.get_with_partial_header("/partial_content.txt")
-
