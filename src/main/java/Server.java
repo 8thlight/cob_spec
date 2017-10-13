46565b67789d8cf18615baa9d5db41d31ebf1a42
Philip Meier <philip.meier06@googlemail.com>
2017-10-13 16:27:57 +0100

    Poll socket readiness during startup

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.net.Socket;
+
 public class Server {
     private String startCommand;
     private String directory;
@@ -19,7 +22,26 @@ public class Server {
     public void startServer() throws Exception {
         String command = startCommand + " -p " + port + " -d " + directory;
         process = Runtime.getRuntime().exec(command);
-        Thread.sleep(2000);
+        while (!hostAvailable()) {
+          Thread.sleep(2000);
+        }
+    }
+
+    public boolean hostAvailable() {
+        Socket s = null;
+        try {
+            s = new Socket("0.0.0.0", Integer.parseInt(port));
+            return true;
+        } catch (IOException ex) {
+            /* ignore */
+        } finally {
+            try {
+                if (s != null) s.close();
+            } catch (IOException ex) {
+                /* ignore */
+            }
+        }
+        return false;
     }
 
     public void stopServer() throws Exception {
